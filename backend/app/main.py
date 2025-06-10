56e7fd68a1091c24ce7a2495236f141e11612b4a
tejas <pangaonkar.svc424@gmail.com>
2025-06-10 11:12:06 +0530

    init

---
+++ backend/app/main.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
+from app import models
+from app.database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine) # it will create all tables and columns
+
+class ChoiceBase(BaseModel):
+    choice_text: str
+    is_correct: bool
+
+class QuestionBase(BaseModel):
+    question_text: str
+    choices: List[ChoiceBase]
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.post("/questions")
+async def create_questions(question: QuestionBase, db: db_dependency):
+    db_question = models.Questions(question_text = question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    for choice in question.choices:
+        db_choice = models.Choices(choice_text = choice.choice_text, is_correct=choice.is_correct, question_id = db_question.id)
+        db.add(db_choice)
+    db.commit()
+
+@app.get("/questions/{question_id}")
+async def read_question(question_id: int, db: db_dependency):
+    result = db.query(models.Questions).filter(models.Questions.id == question_id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail='Question Not Found')
+    return result
+
+@app.get("/choices/{question_id}")
+async def read_choices(question_id: int, db: db_dependency):
+    result = db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
+    if not result:
+        raise HTTPException(status_code=404, detail='Choices Not Found')
+    return result
+
+
+# class Item(BaseModel):
+#     text: str 
+#     is_done: bool = False
+
+# items = []
+
+# @app.get("/")
+# def root():
+#     return {"hello":"world"}
+
+# @app.post("/items")
+# def add_item(item: Item):
+#     items.append(item)
+#     return items
+
+# @app.get("/items",response_model=list[Item])
+# def list_items(limit: int = 10):
+#     return items[0:limit]
+
+# @app.get("/items/{item_id}", response_model=Item)
+# def get_item(item_id : int) -> Item:
+#     if item_id < len(items):
+#         item = items[item_id]
+#         return item
+#     else:
+#         raise HTTPException(status_code=404, detail=f"Item {item_id} Not found")
+    
